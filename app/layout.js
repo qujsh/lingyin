36089be641e3d195143692db340aa7252629e924
sonic <ningmingqaz@126.com>
2025-03-08 15:38:35 +0800

    add electron font test

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Noto+Sans+SC:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={`antialiased`}>
         <main className="flex flex-col h-screen ">
           <div className="absolute inset-0 -z-10 w-full h-screen bg-[url('/river.jpg')] bg-cover bg-top bg-no-repeat opacity-25"></div>
